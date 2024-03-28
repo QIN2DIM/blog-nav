012d6b8f9b2aa5148b600d870871c3fd9b785b35
QIN2DIM <62018067+QIN2DIM@users.noreply.github.com>
2024-03-28 08:57:37 +0800

    Automated backup @ 2024-03-28 08:57:37 Asia/Shanghai

--- push.sh
+++ push.sh
@@ -1,17 +1,33 @@
 export https_proxy=http://127.0.0.1:2081
 
 GREEN=$(tput setaf 2)
+RED=$(tput setaf 1)
 NC=$(tput sgr0)
 
-print(){
-	echo "${GREEN}$1${NC}"
+print_success() {
+    echo "${GREEN}$1${NC}"
 }
 
-cd /e/_GitHubProjects/myProfile/blog-nav || exit
+print_error() {
+    echo "${RED}$1${NC}"
+}
+
+blog_nav_path1="/e/_GitHubProjects/myProfile/blog-nav"
+blog_nav_path2="/d/_GitHubProjects/Sources/blog-nav"
+
+if [ -d "$blog_nav_path1" ]; then
+    cd "$blog_nav_path1" || exit
+elif [ -d "$blog_nav_path2" ]; then
+    cd "$blog_nav_path2" || exit
+else
+    print_error "无法找到 blog-nav 目录，请检查路径是否正确。"
+    exit 1
+fi
+
 hugo --gc --minify --cleanDestinationDir &> /dev/null
-print "[1/2] hugo gc"
+print_success "[1/2] hugo gc"
 
 git add -A &> /dev/null
 git commit -m "Automated backup @ $(date '+%Y-%m-%d %H:%M:%S') Asia/Shanghai"
 git push -f &> /dev/null
-print "[2/2] Push Sources successfully"
+print_success "[2/2] Push Sources successfully"
